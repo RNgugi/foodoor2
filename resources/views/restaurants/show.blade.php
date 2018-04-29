c8d47f2b3c511b0da233ceca3884bdd5539060f9
abhishek47 <operations@trumpetstechnologies.com>
2018-04-29 19:03:35 +0530

    defects

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -151,7 +151,7 @@
                                           </div> 
                                           
                                           <div class="rest-descr" style="padding-left: 23px;">
-                                             <h6 style="{{ $item->description == '' ? 'margin-bottom: 11px;' : '' }}"><a href="#">{{ $item->name }}</a></h6>
+                                             <h6 style="{{ $item->description == '' ? 'margin-bottom: 28px;' : '' }}"><a href="#">{{ $item->name }}</a></h6>
                                              <p>{{ $item->description != '' ? $item->description : ''}}</p>
                                           </div>
                                           <!-- end:Description -->
