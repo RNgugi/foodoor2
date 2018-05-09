25f4279bbe79d6681f98f0beb702000b9431de54
abhishek47 <operations@trumpetstechnologies.com>
2018-05-09 12:08:53 +0530

    changes

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -22,6 +22,7 @@
 
 @if(auth()->user()->can('manage_orders'))
 <li><a href="{{ backpack_url('orders') }}"><i class="fa fa-file-text"></i> <span>Today's Orders</span></a></li>
+<li><a href="{{ backpack_url('history') }}"><i class="fa fa-file-text"></i> <span>Orders History</span></a></li>
 @endif
 
 @if(auth()->user()->can('manage_restaurants'))
