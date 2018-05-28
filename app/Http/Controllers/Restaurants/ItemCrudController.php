db5cdc4599d86fe9b2da1f32234cc915cc0345ef
abhishek47 <operations@trumpetstechnologies.com>
2018-05-28 09:05:04 +0530

    defect

--- app/Http/Controllers/Restaurants/ItemCrudController.php
+++ app/Http/Controllers/Restaurants/ItemCrudController.php
@@ -78,7 +78,6 @@ class ItemCrudController extends CrudController
                 'type' => 'select2_from_array',
                 'options' => [0 => 'Non Veg', 1 => 'Veg'],
                 'allows_null' => false,
-                'default' => 1,
                 'tab' => 'General'
                 // 'allows_multiple' => true, // OPTIONAL; needs you to cast this to array in your model;
             ],
