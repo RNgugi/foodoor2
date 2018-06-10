832af77f415ef589639a6a2e2dbb67bea12847a2
abhishek47 <operations@trumpetstechnologies.com>
2018-06-10 11:47:57 +0530

    changes

--- config/payment.php
+++ config/payment.php
@@ -20,8 +20,8 @@ return [
         'workingKey' => env('CCAVENUE_WORKING_KEY', '3D168E728ED6B65F6AB548F89124B299'),
 
         // Should be route address for url() function
-        'redirectUrl' => env('CCAVENUE_REDIRECT_URL', 'payment/response'),
-        'cancelUrl' => env('CCAVENUE_CANCEL_URL', 'payment/cancel'),
+        'redirectUrl' => env('CCAVENUE_REDIRECT_URL', 'payments/response'),
+        'cancelUrl' => env('CCAVENUE_CANCEL_URL', 'payments/response'),
 
         'currency' => env('CCAVENUE_CURRENCY', 'INR'),
         'language' => env('CCAVENUE_LANGUAGE', 'EN'),
@@ -29,7 +29,7 @@ return [
 
     // Add your response link here. In Laravel 5.* you may use the api middleware instead of this.
     'remove_csrf_check' => [
-        'payment/response',
+        'payments/response',
     ],
 
 ];
